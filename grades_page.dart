9ec919636339021e54c43301ffbcbd34a931136d
Jamshaid352 <ja0320089@gmail.com>
2025-04-09 17:06:55 +0500

    Add files via upload

--- grades_page.dart
+++ grades_page.dart
@@ -8,7 +8,6 @@ class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._init();
   static Database? _database;
   DatabaseHelper._init();
-
   Future<Database> get database async {
     if (_database != null) return _database!;
     _database = await _initDB('grades.db');
